33523ca6fdbc0985ea545d788b97f0ad9dfa3440
Cheol Kim <kr.ckim91@gmail.com>
2016-11-05 17:46:34 -0700

    added adventure section

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -10,8 +10,12 @@ get_header(); ?>
     <div class="banner-hero">
     </div>
 
+   
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+
+             
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
@@ -20,6 +24,41 @@ get_header(); ?>
 			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
+
+        <section class="adventureSection">
+            <h2>Latest Adventures</h2>
+
+          <div class="adventures">
+  
+             <div class="left">
+                <div class="box1">
+                    <a href="#">Getting Back to Nature in a Canoe</a>
+                </div>
+             </div>
+  
+             <div class="right">
+     
+                <div class="topright">
+                    <div class="box2">
+                        <a href="#">A Night with Friends at the Beach</a>
+                    </div>
+                </div>
+     
+             <div class="box34">
+                <div class="box3">
+                    <a href="#">Taking in the View at Big Mountain</a>
+                </div>
+                <div class="box4">
+                    <a href="#">Star-Gazing at the Night Sky</a>
+                </div>
+            <div>
+                
+           </div>
+  
+         </div>
+
+         </section>
+
 	</div><!-- #primary -->
 
 
