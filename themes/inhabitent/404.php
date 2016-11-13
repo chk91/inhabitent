9b331cd6ec07f4335dad4447520c5a48ed727013
Cheol Kim <kr.ckim91@gmail.com>
2016-11-12 20:36:16 -0800

    made and styled 404 page

--- themes/inhabitent/404.php
+++ themes/inhabitent/404.php
@@ -9,8 +9,8 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="primary" class="content-areahome">
+		<main id="main" class="site-mainhome" role="main">
 
 			<section class="error-404 not-found">
 				<header class="page-header">
@@ -50,6 +50,8 @@ get_header(); ?>
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
+		<?php get_sidebar(); ?>
+
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
