6bc51d416a414da45e116a879c97476dfd9d5e2d
Cheol Kim <kr.ckim91@gmail.com>
2016-11-12 19:49:19 -0800

    finished styling find us page and added comment section

--- themes/inhabitent/single.php
+++ themes/inhabitent/single.php
@@ -44,6 +44,13 @@ get_header(); ?>
 
 			<?php the_posts_navigation(); ?>
 
+			<?php
+                // If comments are open or we have at least one comment, load up the comment template.
+                if ( comments_open() || get_comments_number() ) :
+                    comments_template();
+                endif;
+            ?>
+
 		<?php else : ?>
 
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
